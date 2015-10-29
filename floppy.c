ffdac6cc6960854abf72fbd1867356cd551032ec
yvnewman <littlelopeep@gmail.com>
2015-10-28 20:28:37 -0400

    second commit

--- floppy.c
+++ floppy.c
@@ -2,53 +2,10 @@
 #include <stdlib.h>
 #include <floppy.h>
 
-int loop;
-
-int command()
-{
-int i = 0;
-
-char input[60];
-const char *fmount = "fmount";
-const char *funmount = "funmount";
-const char *exit = "exit";
-
-printf("f: ");
-fgets(input, 60, stdin);
-
-   	 if(strncmp(input, fmount, 6) == 0)
-	{
-            printf("wants to mount\n");
-		//fork and call mounting program here pass in all of input so the 
-		//second arg can be accessed with argv!! TODO
-    	}  
-
-   	 if(strncmp(input, funmount, 8) == 0)
-	{
-            printf("wants to unmount\n");
-		//fork and call mounting program here pass in all of input so the 
-		//second arg can be accessed with argv!! TODO
-    	}  
-
-   	 if(strncmp(input, exit, 4) == 0)
-	{
-            printf("wants to exit\n");
-		
-	return(0);
-    	} 
-	
-	//case statements for the rest of the commands, make in floppy.c TODO
-   	
-
-
- return(1);
-
-}
+unsigned short sector_size;
 
 
 //this is code for mounting the image TODO
-/*unsigned short sector_size;
-
 int fmount(char *img)
 {
 
@@ -66,6 +23,4 @@ char *fd = img;
 
 return 0;
 }
-
-*/
 
--- main.c
+++ main.c
@@ -2,16 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern int loop;
-
 main()
 {
-loop = 1;
 
+int loop = 1;
+int i = 0;
+extern unsigned int sector_size;
+char input[60];
+const char *fmount = "fmount";
+const char *funmount = "funmount";
+const char *quit = "quit";
+
+	//call up prompt and take input until user types "quit"
 	while(loop == 1)
 	{
-	loop = command();
+
+		printf("f: ");
+		fgets(input, 60, stdin);
+
+	   	 if(strncmp(input, fmount, 6) == 0)
+		{
+			printf("wants to mount\n");
+
+	    	}  
+
+	   	 if(strncmp(input, funmount, 8) == 0)
+		{
+		    printf("wants to unmount\n");
+
+	    	}  
+
+	   	 if(strncmp(input, quit, 4) == 0)
+		{
+		    printf("wants to quit\n");
+		
+		loop = 0;
+	    	} 
 	}
+	
+/*
+TODO
+
+help: show commands supported 5%
+
+structure: list structure 10%
+	num FAT, num sectors used by FAT, num sectors/cluster, num ROOT entries, num bytes/sector
+	sector # ------ sector types
+
+traverse -1: list contents 20%!!!
+	when -1 is turned on, it's a long list
+	sort and display, show <DIR> for dirs
+
+showsector 10: show hex dump of a sector (512 bytes) 10%
+
+showfat: show contents of first 256 entries of FAT table as hex dump 15%
+
+showfile filename: show content of target file as hex dump 15%
+
+need to finish: writeup, mount and unmount program/fork thingies, write-up, clean up and comment
+*/
+
 }
+
 
 
